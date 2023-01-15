4a1bcf64ec9869cf323ced316dddd741c1cbbfa8
oz527 <118091285+oz527@users.noreply.github.com>
2023-01-15 20:03:30 +0900

    todolist

--- js/todo.js
+++ js/todo.js
@@ -4,12 +4,21 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
-  console.log(toDoInput.value);
+  //   const newToDo는 input의 value를 비우기 전의 값을 나타내는 string
   toDoInput.value = "";
-  console.log(newToDo, toDoInput.value);
+  paintToDo(newToDo);
+  //   그런 다음 그 입력값을 paintToDo에 넣어서 호출
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
