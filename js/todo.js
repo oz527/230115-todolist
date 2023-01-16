c60c6601a8467a5c1a83e9546aaaa989fba3d161
oz527 <118091285+oz527@users.noreply.github.com>
2023-01-16 17:12:23 +0900

    todo

--- js/todo.js
+++ js/todo.js
@@ -4,11 +4,20 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newToDo;
+  const button = document.createElement("button");
+  button.innerText = "✖️";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
